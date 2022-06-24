ee57f57dfcf106190b8c69adc3ade770270d4c97
paurana ubuntu <f20200803@pilani.bits-pilani.ac.in>
2022-06-24 18:27:46 +0530

    updated config

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    #set BASE16_SHELL "$HOME/.config/base16-shell/"
+    #source "$BASE16_SHELL/profile_helper.fish"
 end
 
 alias ls=exa
