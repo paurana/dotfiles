df0cd7b3cee789702de0e8f90cbe64d9bcc9615e
paurana ubuntu <f20200803@pilani.bits-pilani.ac.in>
2022-06-26 03:21:06 +0530

    jonhoo stream

--- fish/config.fish
+++ fish/config.fish
@@ -9,5 +9,19 @@ end
 export PATH="$HOME/.cargo/bin:$PATH"
 export WINIT_UNIX_BACKEND=x11
 
-alias ls=exa
-alias tmux="tmux -u"
+# alias ls=exa
+# alias tmux="tmux -u"
+
+abbr -a tmux 'tmux -u'
+
+# better way to bind exa, also better bindings
+if command -v exa > /dev/null
+	abbr -a l 'exa'
+	abbr -a ls 'exa'
+	abbr -a ll 'exa -l'
+	abbr -a lll 'exa -la'
+else
+	abbr -a l 'ls'
+	abbr -a ll 'ls -l'
+	abbr -a lll 'ls -la'
+end
