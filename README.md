1809909f4180f1afb834579a8f1b50443b71d834
paurana ubuntu <f20200803@pilani.bits-pilani.ac.in>
2022-07-29 10:48:54 +0530

    polybar configured

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # configs
 
-![screenshot](img/img1.png?raw=true)
+![screenshot1](img/img1.png?raw=true)
+
+![screenshot2](img/img2.png?raw=true)
---
+++ polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
