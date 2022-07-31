acad78192df843d2663f7490c3e480f759092872
paurana ubuntu <f20200803@pilani.bits-pilani.ac.in>
2022-07-31 21:18:48 +0530

    replaced bash loop with cron

--- i3/battery.sh
+++ i3/battery.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# TODO: use cron instead of while loop
-while true; do
-        battery=$(cat /sys/class/power_supply/BAT1/capacity)
-        status=$(cat /sys/class/power_supply/BAT1/status)
+battery=$(cat /sys/class/power_supply/BAT1/capacity)
+status=$(cat /sys/class/power_supply/BAT1/status)
 
-        if [ "$status" == "Discharging" ] && [ "$battery" -lt 20 ]; then
-                notify-send "Battery Low. Please plug in your charger."
-        elif [ "$status" == "Charging" ] && [ "$battery" -gt 95 ]; then
-                notify-send "Battery Full. Please unplug your charger."
-        fi
-        sleep 30
-done
+# removed the infinite loop and set up a cron job to run this script every minute
+if [ "$status" == "Discharging" ] && [ "$battery" -lt 20 ]; then
+        XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send "Battery Low" "Please plug in your charger."
+elif [ "$status" == "Charging" ] && [ "$battery" -gt 95 ]; then
+        XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send "Battery Full" "Please unplug your charger."
+fi
 
