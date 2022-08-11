daa251606fa5cc236b57f51b4ff5acb36e173f3f
paurana ubuntu <f20200803@pilani.bits-pilani.ac.in>
2022-08-11 17:33:12 +0530

    tmux on alacritty start, i3lock screen customized

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,8 @@
 if status is-interactive
-    #set BASE16_SHELL "$HOME/.config/base16-shell/"
-    #source "$BASE16_SHELL/profile_helper.fish"
 end
-
-# set fish_function_path $fish_function_path ~/.config/fish/plugin-foreign-env/functions
-# fenv source ~/.profile
 
 export PATH="$HOME/.cargo/bin:$PATH"
 export WINIT_UNIX_BACKEND=x11
-
-# alias ls=exa
-# alias tmux="tmux -u"
 
 abbr -a tmux 'tmux -u'
 
@@ -26,3 +18,9 @@ else
 	abbr -a lll 'ls -la'
 end
 
+function _trap_exit
+        set name $(tmux display-message -p '#S')
+        tmux kill-session -t name
+end
+
+trap _trap_exit EXIT
---
+++ i3/scripts/lock.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# take screenshot
+
+import -window root /tmp/screenshot.png
+
+# blur it
+
+convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
+
+rm /tmp/screenshot.png
+
+# lock the screen
+
+i3lock -i /tmp/screenshotblur.png
